39769b7b03dde7e474c67cadb76992fc696d99d5
Tuysh <angelmacloviomorales@gmail.com>
2024-12-01 20:58:00 -0600

    (añadido): Módulo de utilidades para funciones comunes
    Se añadió este archivo para centralizar funciones auxiliares que se utilizarán en distintas partes del proyecto.
    - Proporciona herramientas reutilizables para simplificar la lógica en otros módulos.
    - Facilita el mantenimiento al tener un único punto para ajustar funciones genéricas.
    
    Porque, seamos sinceros, todos necesitamos un pequeño arsenal de funciones útiles para no reinventar la rueda. 🚀

---
+++ src/Utils.elm
@@ -0,0 +1,75 @@
+module Utils exposing (centeredText, colors, styles, toElmUiColor)
+
+import Color
+import Element as E
+import Element.Background as Background
+import Element.Border as Border
+import Html.Attributes
+
+
+toElmUiColor : Color.Color -> E.Color
+toElmUiColor color =
+    let
+        { red, green, blue, alpha } =
+            Color.toRgba color
+    in
+    E.rgba red green blue alpha
+
+
+colors : { white : E.Color, lightGrey : E.Color, darkGrey : E.Color, black : E.Color }
+colors =
+    { white =
+        E.rgb 0.97 0.97 0.97
+    , lightGrey =
+        E.rgb 0.87 0.87 0.87
+    , darkGrey =
+        toElmUiColor Color.darkGrey
+    , black =
+        toElmUiColor Color.black
+    }
+
+
+styles : { button : List (E.Attr () msg), tab : List (E.Attr () a), padTop10 : E.Attribute b, padTop20 : E.Attribute c, padBottom20 : E.Attribute d, padBottom10 : E.Attribute e }
+styles =
+    { button =
+        [ Background.color colors.lightGrey
+        , E.mouseOver
+            [ Background.color colors.white ]
+        , E.paddingXY 10 5
+        , E.width <| E.px 150
+        , Border.widthXY 2 1
+        , Border.color colors.darkGrey
+        ]
+    , tab =
+        [ Background.color colors.lightGrey
+        , E.mouseOver
+            [ Background.color colors.white ]
+        , Border.widthEach
+            { top = 1
+            , left = 1
+            , bottom = 0
+            , right = 1
+            }
+        , Border.color colors.darkGrey
+        , Border.roundEach
+            { topLeft = 10
+            , topRight = 10
+            , bottomLeft = 0
+            , bottomRight = 0
+            }
+        ]
+    , padTop10 =
+        E.htmlAttribute <| Html.Attributes.style "margin-top" "10px"
+    , padTop20 =
+        E.htmlAttribute <| Html.Attributes.style "margin-top" "20px"
+    , padBottom20 =
+        E.htmlAttribute <| Html.Attributes.style "margin-bottom" "20px"
+    , padBottom10 =
+        E.htmlAttribute <| Html.Attributes.style "margin-bottom" "10px"
+    }
+
+
+centeredText : String -> E.Element msg
+centeredText text =
+    E.el [ E.centerX ] <|
+        E.text text
