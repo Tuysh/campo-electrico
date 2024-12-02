adeede9e5e7ee8ee4db0f08dcd47af627f2b32e3
Tuysh <angelmacloviomorales@gmail.com>
2024-12-01 21:47:43 -0600

    (arreglado): Refactorización de lógica y eliminación de código redundante
    Se refactorizó el código para mejorar la organización y eliminar funciones redundantes que estaban duplicando el manejo de estados o simulaciones. Los cambios incluyen:
    - Se simplificaron las actualizaciones del modelo, consolidando la lógica de la simulación y la interacción del cursor.
    - Se optimizó la gestión de los eventos de la vista y los eventos del mouse.
    - Se eliminaron varias funciones auxiliares no necesarias como updateActiveSimulationWithMsg, updateIsInteractionEnabled, y mouseDown/mouseUp, que estaban redundantes al realizar tareas que ya se gestionaban en otras partes.
    - Además, se mejoró la función handleCursorClick para simplificar la lógica de detección de clics y eliminación de cargas.
    
    Esto mejora la claridad y mantenibilidad del código.

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,27 @@
 module Main exposing (main)
 
 import Browser
-import Browser.Events
-import Element as E
+import Browser.Events exposing (onResize)
+import Browser.Dom exposing (getViewport)
+import Element as E exposing (fill, centerX)
 import Element.Background as Background
 import Html exposing (Html)
-import Html.Attributes
-import List.Extra
-import Math.Vector2 as Vector2
-import Simulation
+import Html.Attributes exposing (style)
+import Math.Vector2 as Vector2 exposing (vec2)
+import Simulation exposing (Model, Msg, Position, Sign)
 import Task
 import Utils exposing (toElmUiColor)
-import Browser.Dom
 
 type alias Model =
     { activeSimulation : Simulation.Model
-    , simulationWidth : Float
-    , simulationHeight : Float
     , cursor : Cursor
     , isMouseDown : Bool
     , isInteractionEnabled : Bool
-    , isMobile : Bool
     }
 
 type Cursor
     = Selector
-    | Painter Simulation.Sign
+    | Painter Sign
     | Deleter
 
 type Msg
@@ -34,271 +30,153 @@ type Msg
     | WindowResized Int Int
     | UpdateActiveSimulationState
     | UpdateCursor Cursor
-    | DrawCharges Simulation.Position
-    | CursorClicked Simulation.Position
+    | DrawCharges Position
+    | CursorClicked Position
     | MouseDown
     | MouseUp
 
-
-defaultSimulationWidth : Float
-defaultSimulationWidth =
-    1200
-
-
-defaultSimulationHeight : Float
-defaultSimulationHeight =
-    750
-
-
 init : ( Model, Cmd Msg )
 init =
-    let
-        defaultActiveSimulation =
-            Simulation.init defaultSimulationWidth defaultSimulationHeight
-
-        model =
-            { activeSimulation = defaultActiveSimulation
-            , simulationWidth = defaultSimulationWidth
-            , simulationHeight = defaultSimulationHeight
-            , cursor = Selector
-            , isMouseDown = False
-            , isInteractionEnabled = True
-            , isMobile = False
-            }
-    in
-    ( model
-    , Task.perform GotViewport Browser.Dom.getViewport
+    ( { activeSimulation = Simulation.init 1200 750
+      , cursor = Selector
+      , isMouseDown = False
+      , isInteractionEnabled = True
+      }
+    , Task.perform GotViewport getViewport
     )
 
 view : Model -> Html Msg
 view model =
     E.layout
-        [ E.width E.fill
-        , E.height E.fill
+        [ E.width fill
+        , E.height fill
         , Background.color <| toElmUiColor model.activeSimulation.settings.colors.background
-        , E.htmlAttribute <| Html.Attributes.style "touch-action" "none"
+        , E.htmlAttribute <| style "touch-action" "none"
         ]
-    <|
-        E.el
-            [ E.centerX ]
+        (E.el [ centerX ]
             (E.html (Html.map SimulationMsg <| Simulation.view model.activeSimulation))
-
+        )
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
-
         SimulationMsg msg ->
-            updateActiveSimulationWithMsg msg model
+            let
+                ( newSimulation, cmd ) =
+                    Simulation.update msg model.activeSimulation
+            in
+            ( { model | activeSimulation = newSimulation }
+            , Cmd.map SimulationMsg cmd
+            )
 
         GotViewport viewport ->
-            ( updateSimulationSize viewport.viewport.width viewport.viewport.height model, Cmd.none )
+            ( updateSimulationSize viewport.viewport.width viewport.viewport.height model
+            , Cmd.none
+            )
 
-        WindowResized newWidth newHeight ->
-            ( updateSimulationSize (toFloat newWidth) (toFloat newHeight) model, Cmd.none )
+        WindowResized width height ->
+            ( updateSimulationSize (toFloat width) (toFloat height) model
+            , Cmd.none
+            )
 
         UpdateActiveSimulationState ->
-            ( updateActiveSimulationState model, Cmd.none )
+            ( { model 
+                | activeSimulation =
+                    let 
+                        sim = model.activeSimulation
+                        nextState = 
+                            case sim.state of
+                                Simulation.Running -> Simulation.Resting
+                                Simulation.Resting -> Simulation.Running
+                    in
+                    { sim | state = nextState }
+              }
+            , Cmd.none
+            )
 
         UpdateCursor newCursor ->
-            ( updateCursor newCursor model, Cmd.none )
+            ( { model 
+                | cursor = newCursor
+                , isInteractionEnabled = newCursor /= Deleter 
+              }
+            , Cmd.none
+            )
 
         DrawCharges position ->
-            ( drawCharges position model, Cmd.none )
+            ( case model.cursor of
+                Painter sign ->
+                    if model.isMouseDown then
+                        { model | activeSimulation = Simulation.addCharge sign position model.activeSimulation }
+                    else
+                        model
+                _ ->
+                    model
+            , Cmd.none
+            )
 
         CursorClicked position ->
-            ( cursorClicked position model, Cmd.none )
+            ( handleCursorClick position model
+            , Cmd.none 
+            )
 
         MouseDown ->
-            ( mouseDown model, Cmd.none )
+            ( { model | isMouseDown = True }, Cmd.none )
 
         MouseUp ->
-            ( mouseUp model, Cmd.none )
+            ( { model | isMouseDown = False }, Cmd.none )
 
-updateActiveSimulationWithMsg : Simulation.Msg -> Model -> ( Model, Cmd Msg )
-updateActiveSimulationWithMsg msg model =
-    let
-        ( newSimulation, cmd ) =
-            Simulation.update msg model.activeSimulation
-    in
-    ( updateActiveSimulation newSimulation model
-    , Cmd.map SimulationMsg cmd
-    )
-
-
-updateActiveSimulation : Simulation.Model -> Model -> Model
-updateActiveSimulation newActiveSimulation model =
-    { model
-        | activeSimulation =
-            newActiveSimulation
-    }
-
-updateIsInteractionEnabled : Bool -> Model -> Model
-updateIsInteractionEnabled isInteractionEnabled model =
-    let
-        oldSimulation =
-            model.activeSimulation
-
-        newSimulation =
-            { oldSimulation
-                | isInteractionEnabled =
-                    isInteractionEnabled
-            }
-    in
-    updateActiveSimulation
-        newSimulation
-        { model | isInteractionEnabled = isInteractionEnabled }
-
-
-updateSimulationSize : Float -> Float -> Model -> Model
-updateSimulationSize newWidth newHeight model =
-    let
-        isMobile =
-            min newWidth newHeight < 500
-
-        newHeightAdapted =
-            if isMobile then
-                newHeight - 60
-
-            else
-                newHeight - 150
-
-        updateSize =
-            \simulation ->
-                if simulation.width /= newWidth || simulation.height /= newHeightAdapted then
-                    Simulation.init newWidth newHeightAdapted
-
-                else
-                    simulation
-    in
-    { model
-        | simulationWidth =
-            newWidth
-        , simulationHeight =
-            newHeightAdapted
-        , activeSimulation =
-            updateSize model.activeSimulation
-        , isMobile =
-            isMobile
-    }
-
-
-updateActiveSimulationState : Model -> Model
-updateActiveSimulationState model =
-    let
-        activeSimulation =
-            model.activeSimulation
-
-        nextState =
-            case activeSimulation.state of
-                Simulation.Running ->
-                    Simulation.Resting
-
-                Simulation.Resting ->
-                    Simulation.Running
-    in
-    { model
-        | activeSimulation =
-            { activeSimulation
-                | state =
-                    nextState
-            }
-    }
-
-
-updateCursor : Cursor -> Model -> Model
-updateCursor newCursor model =
-    let
-        oldIsInteractionEnabled =
-            model.isInteractionEnabled
-
-        newModel =
-            updateIsInteractionEnabled (newCursor /= Deleter)
-                { model
-                    | cursor = newCursor
-                }
-    in
-    { newModel
-        | isInteractionEnabled = oldIsInteractionEnabled
-    }
-
-drawCharges : Simulation.Position -> Model -> Model
-drawCharges position model =
+handleCursorClick : Position -> Model -> Model
+handleCursorClick position model =
     case model.cursor of
         Painter sign ->
-            if model.isMouseDown then
-                updateActiveSimulation
-                    (Simulation.addCharge sign position model.activeSimulation)
-                    model
-
-            else
-                model
-
-        _ ->
-            model
-
-
-cursorClicked : Simulation.Position -> Model -> Model
-cursorClicked position model =
-    case model.cursor of
-        Painter sign ->
-            updateActiveSimulation
-                (Simulation.addCharge sign position model.activeSimulation)
-                model
-
+            { model | activeSimulation = Simulation.addCharge sign position model.activeSimulation }
+            
         Deleter ->
             let
-                clickedChargedId =
-                    List.Extra.findMap
-                        (\field ->
-                            if
-                                Vector2.distance
-                                    (Vector2.vec2 (Tuple.first position) (Tuple.second position))
-                                    field.source.position
-                                    <= field.source.r
-                            then
-                                Just field.source.id
-
-                            else
-                                Nothing
+                dist p1 p2 = Vector2.distance (vec2 (Tuple.first p1) (Tuple.second p1)) p2
+                
+                findClickedCharge =
+                    List.foldl
+                        (\field maybeId ->
+                            case maybeId of
+                                Just id -> Just id
+                                Nothing ->
+                                    if dist position field.source.position <= field.source.r then
+                                        Just field.source.id
+                                    else
+                                        Nothing
                         )
+                        Nothing
                         model.activeSimulation.fields
             in
-            case clickedChargedId of
+            case findClickedCharge of
                 Just id ->
-                    updateActiveSimulation
-                        (Simulation.deleteCharge id model.activeSimulation)
-                        model
-
+                    { model | activeSimulation = Simulation.deleteCharge id model.activeSimulation }
                 Nothing ->
                     model
-
+                    
         _ ->
             model
 
-
-mouseDown : Model -> Model
-mouseDown model =
-    { model
-        | isMouseDown =
-            True
-    }
-
-
-mouseUp : Model -> Model
-mouseUp model =
-    { model
-        | isMouseDown =
-            False
+updateSimulationSize : Float -> Float -> Model -> Model
+updateSimulationSize width height model =
+    { model 
+        | activeSimulation = 
+            let
+                newHeight = height - 150
+            in
+            if model.activeSimulation.width /= width || model.activeSimulation.height /= newHeight then
+                Simulation.init width newHeight
+            else
+                model.activeSimulation
     }
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ Sub.map SimulationMsg <| Simulation.subscriptions model.activeSimulation
-        , Browser.Events.onResize WindowResized
+        [ Sub.map SimulationMsg (Simulation.subscriptions model.activeSimulation)
+        , onResize WindowResized
         ]
-
 
 main : Program () Model Msg
 main =
